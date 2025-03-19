923ab2ce1028459acdf1af6def597039048b6645
vuminhhoangg <vmh06122003@gmail.com>
2025-03-20 00:08:19 +0700

    first commit

---
+++ back_end/src/main/java/com/example/e_store/EStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.e_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EStoreApplication.class, args);
+	}
+
+}
---
+++ back_end/src/test/java/com/example/e_store/EStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.e_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
